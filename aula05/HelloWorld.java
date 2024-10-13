1a0ed6fdd962839584b8f91380e67c88c5d764b8
Sergio Nibon <snibonjr@gmail.com>
2024-10-13 14:24:12 -0300

    first commit

---
+++ aula05/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+    public static void main () {
+        main(null);
+    }
+
+    public static void main (String[] args) {
+		
+		System.out.println("Hello World");
+	}
+}
+
+// A classe começa com letra maiúscula e segue o camel case
---
+++ curso-java-loiane-sublime/labs/MeuExemploJava.java
@@ -0,0 +1,9 @@
+class MeuExemploJava {
+	public static void main(String[] args) {
+		System.out.println("Tetes de exibição");
+		System.out.println("Exibindo nomes com acentuação");
+		System.out.println("Maçã, romã, irmã");
+		System.out.println("%%, &&, $$, ##, @@");
+
+	}
+}
