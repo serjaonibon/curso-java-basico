68a45f68c4b6c0e4bf58c9b21171db1e263eec09
Sergio Nibon <snibonjr@gmail.com>
2024-10-24 21:31:35 -0300

    Atualização em 24/10/2024

--- cursoJavaBasico/src/com/loiane/cursojava/aula15/lab/Exer05.java
+++ cursoJavaBasico/src/com/loiane/cursojava/aula15/lab/Exer05.java
@@ -1,9 +1,29 @@
 package com.loiane.cursojava.aula15.lab;
 
+import java.util.Scanner;
+
 public class Exer05 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scan = new Scanner(System.in);
+		
+		
+		System.out.println("Informe a primeira nota do aluno: ");
+		double nota1 = scan.nextDouble();
+		
+		System.out.println("Informe a segunda nota do aluno: ");
+		double nota2 = scan.nextDouble();		
+				
+		double media = (nota1 + nota2) / 2;
+		
+		if (media < 7 ) {
+			System.out.println("Sua média foi " + media + ". Você está REPROVADO!");
+		} else if (media >= 7 && media < 10) {
+			System.out.println("Sua média foi " + media + ". Você está APROVADO!");
+		} else if (media == 10) {
+			System.out.println("Sua média foi " + media + ". Você está APROVADO com a nota MÁXIMA!");
+		}
+		
 
 	}
 
